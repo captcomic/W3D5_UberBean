f90986817fe869c7be381590cbaf4d640108c9a2
Yeashfi Hasan <capt.comic@gmail.com>
2018-06-22 14:38:22 -0400

    Initial Commit

---
+++ UberBean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberBean
+//
+//  Created by Fariha on 6/22/18.
+//  Copyright © 2018 Bootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberBean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberBean
+//
+//  Created by Fariha on 6/22/18.
+//  Copyright © 2018 Bootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
